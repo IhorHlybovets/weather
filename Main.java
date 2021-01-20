63ee1003b0fb221129b10be7a1fa2c678d1b3552
IhorHlybovets <66537078+IhorHlybovets@users.noreply.github.com>
2021-01-20 23:00:07 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++){
+            System.out.print("Номер элемента: " + i);
+        }
+    }
+}
